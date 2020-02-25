c6b48f69cb81364568c6cb8f724902ad2920d182
tashy-allie <61415220+tashy-allie@users.noreply.github.com>
2020-02-25 13:21:44 +0200

    Add files via upload

---
+++ height.py
@@ -0,0 +1,12 @@
+height1 =int(input("Enter height :- "))
+height2 =int(input("Enter height :- "))
+height3= int(input("Enter height :- "))
+height4= int(input("Enter height :- "))
+height5= int(input("Enter height :- "))
+height6= int(input("Enter height :- "))
+height7= int(input("Enter height :- "))
+height8= int(input("Enter height :- "))
+height9= int(input("Enter height :- "))
+height10= int(input("Enter height :- "))
+avgNum = (height1+height2+height3+height4+height5+height6+height7+height8+height9+height10)/10.
+print("The average of the numbers is = ",avgNum)
